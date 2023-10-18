8493f033e2dd51d60fa7fac6fc3d31faeb984297
harvey.donnelly <harveydonnelly404@gmail.com>
2023-10-18 19:41:50 -0400

    error handling for gen schedule

--- scheduler_api/api/controllers/scheduleController.js
+++ scheduler_api/api/controllers/scheduleController.js
@@ -5,8 +5,15 @@ function generateSchedule(req, res) {
     const camp_id = req.body.camp_id;
     const start_time = new Date(req.body.start_time);
     const name = req.body.name;
-
-    const new_schedule = scheduleService.generateSchedule(camp_id, start_time, name);
+    
+    var new_schedule
+    try {
+        new_schedule = scheduleService.generateSchedule(camp_id, start_time, name);
+    } catch(err) {
+        return {
+            error: err
+        }
+    }
 
     return {
         schedule: new_schedule
--- scheduler_api/api/services/scheduleService.js
+++ scheduler_api/api/services/scheduleService.js
@@ -4,7 +4,14 @@ const Schedule = require('../entities/Schedule');
 const crypto = require('crypto');
 
 function generateSchedule(camp_id, start_time, name) {
-    var activities = db.getCampActivities(camp_id);
+    var activities;
+    
+    try {
+        activities = db.getCampActivities(camp_id);
+    } catch(err) {
+        throw Error('Generate Schedule Failed: ' + err);
+    }
+
     const schedule_id = crypto.randomUUID();
 
     var new_schedule = new Schedule(schedule_id,
